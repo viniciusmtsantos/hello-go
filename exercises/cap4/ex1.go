0c6ca0a65d145dba50196d92fbb18d11c4f6e231
Vinicius Santos <vinicius.santosmatheus1@gmail.com>
2022-11-01 15:32:23 -0300

    ADD

---
+++ exercises/cap4/ex1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 1024
+	fmt.Printf("O numero %d é:\n", x)
+	fmt.Printf("Em Decimal: %b\n", x)
+	fmt.Printf("Em Hexadecimal: %#x\n", x)
+}
+
+// Escreva um programa que mostre um numero em decimal, binário e hexadecimal
