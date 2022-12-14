4faa5909afa7898510964d026c37aeccf78a6682
Vinicius Santos <vinicius.santosmatheus1@gmail.com>
2022-12-14 13:10:50 -0300

    ADD

---
+++ exercises/cap5/ex6.go
@@ -0,0 +1,17 @@
+// Crie um programa que utilize a declaração switch, sem switch statement especificado
+
+package main
+
+import "fmt"
+
+func main() {
+	x := 1
+	switch {
+	case x == 1:
+		fmt.Printf("Uou, X é %v", x)
+	case x == 2:
+		fmt.Printf("Uou, X é %v", x)
+	case x == 3:
+		fmt.Printf("Uou, X é %v", x)
+	}
+}
